47a66f2f2c971da7da8d40a47dd82ba09387cca1
xdrop <pastosdev@gmail.com>
2016-09-04 21:43:06 +0300

    [Feature] Delete to EOL + Reformat

--- sublimefilter.py
+++ sublimefilter.py
@@ -3,23 +3,24 @@ import re
 
 scan_limit = 1000
 
+
 class SublimeFilter:
 
-	def __init__(self, view, passthrough):
-		self.passthrough = passthrough
-		self.view = view
+    def __init__(self, view, passthrough):
+        self.passthrough = passthrough
+        self.view = view
 
-	def is_pass(self, char):
-		return char not in self.passthrough
+    def is_pass(self, char):
+        return char not in self.passthrough
 
-	def suffix(self, point):
-		for i in range(0, scan_limit):
-			next_point = point + i
-			if not self.is_pass(get_char(self.view, next_point)):
-				return next_point
+    def suffix(self, point):
+        for i in range(0, scan_limit):
+            next_point = point + i
+            if not self.is_pass(get_char(self.view, next_point)):
+                return next_point
 
 
 def get_char(view, point):
-	one_char = sublime.Region(point, point + 1)
-	char = view.substr(one_char)
-	return char
+    one_char = sublime.Region(point, point + 1)
+    char = view.substr(one_char)
+    return char
